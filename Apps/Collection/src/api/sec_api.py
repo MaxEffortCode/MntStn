af93638d473149e0c82ae0bbdd71eff76d6c0b77
maxeffortcode <bultman82@gmail.com>
2022-06-01 13:03:33 -0500

    Created a 494 file downloader

--- Apps/Collection/src/api/sec_api.py
+++ Apps/Collection/src/api/sec_api.py
@@ -63,6 +63,13 @@ class SecAPI:
         LOGGER.info(f"Performing GET on: {url}")
         return response
     
+    def get494FilingForCompanyApi(self, companyInfo):
+        url = f"{self.baseUrl}/Archives/{companyInfo[4]}"
+        url = url.replace('-','').replace('.txt', '/index.json')
+        response = requests.get(url, headers=self.header)
+        LOGGER.info(f"Performing GET on: {url}")
+        return response
+    
     def get4AFilingForCompanyApi(self, companyInfo):
         url = f"{self.baseUrl}/Archives/{companyInfo[4]}"
         url = url.replace('-','').replace('.txt', '/index.json')

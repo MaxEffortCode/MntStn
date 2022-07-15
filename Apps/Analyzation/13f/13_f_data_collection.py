ae08abd319249a6710c4db202d383a6c2fa14bdb
Max B <bultman82@gmail.com>
2022-07-15 18:25:01 -0500

    merged ciks to tickers need to work on natching better

--- Apps/Analyzation/13f/13_f_data_collection.py
+++ Apps/Analyzation/13f/13_f_data_collection.py
@@ -1,7 +1,9 @@
+from email import header
 from tickers import get_source
 import matplotlib.pyplot as plt
 import os
 import csv
+import re
 import time
 import pandas
 
@@ -9,20 +11,6 @@ tracked_companies = []
 
 def get_ticker(company_name):
     soup = get_source(company_name)
-    print(soup)
-
-def clean_company_name(company_name):
-    company_name = company_name.upper()
-    company_name = company_name.replace(" ", "")
-    company_name = company_name.replace(".", "")
-    company_name = company_name.replace(",", "")
-    company_name = company_name.replace("-", "")
-    company_name = company_name.replace("&", "AND")
-    company_name = company_name.replace("(", "")
-    company_name = company_name.replace(")", "")
-    company_name = company_name.replace("CORPORATION", "CORP")
-    
-    return company_name
 
 
 def clear_csv(csv_file):
@@ -69,12 +57,12 @@ def collect_13_f_companies_and_shares():
     return [sub for sub in data_locations]
 
 
-def create_full_qtr_list_of_13f(file_arr):
-    with open('13f_collection.csv', 'a', newline='') as csv_header:
+def create_full_qtr_list_of_13f(file_arr, path):
+    with open(path, 'a', newline='') as csv_header:
         csv_writer = csv.writer(csv_header, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(['nameOfIssuer', 'cusip', 'sshPrnamtType', 'putCall', 'investmentDiscretion',\
-            'otherManager', 'soleVotingAuthority', \
-            'sharedVotingAuthority','noneVotingAuthority'])
+        csv_writer.writerow(['nameOfIssuer','cusip','value','shares','sshPrnamtType',\
+            'putCall','investmentDiscretion','otherManager','soleVotingAuthority','sharedVotingAuthority','noneVotingAuthority'
+            ])
         
     
     for file in file_arr:
@@ -83,8 +71,9 @@ def create_full_qtr_list_of_13f(file_arr):
                 csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
                 next(csv_reader)
                 for row in csv_reader:
+                    row[1] = row[1].upper()
                     try:
-                       with open('13f_collection.csv', 'a', newline='') as csvfile:
+                       with open(path, 'a', newline='') as csvfile:
                             csv_writer = csv.writer(csvfile, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
                             csv_writer.writerow(row)
                     except ValueError as e:
@@ -96,24 +85,114 @@ def create_full_qtr_list_of_13f(file_arr):
         except(IndexError):
             continue
 
-    return '13f_collection.csv'
+    return path
 
+def read_all_copmany_names_from_edgar_master(edgar_path, save_path):
+    with open(save_path, 'w', newline='') as csv_header:
+        csv_writer = csv.writer(csv_header, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['nameOfIssuer','cusip','value','shares','sshPrnamtType',\
+            'putCall','investmentDiscretion','otherManager','soleVotingAuthority','sharedVotingAuthority','noneVotingAuthority'
+            ])
     
+    with open(edgar_path, 'r', newline='') as edgar_file:
+        with open(save_path, 'r+', newline='') as new_file:
+            try:
+                for x in range(12):
+                    #ignore intro lines
+                    next(edgar_file)
+                
+                last_row = None
+                for row in edgar_file:
+                    row_split = row.split("|")
+                    try:
+                        if row_split[1] == last_row:
+                            continue
+
+                        last_row = row_split[1]
+                        
+                        csv_writer = csv.writer(new_file, delimiter=',',
+                            escapechar=' ', quoting=csv.QUOTE_NONE)
+                        
+                        company_name = re.sub('"', '', row_split[1])
+                        company_name = re.sub(',', '', row_split[1])
+                        cik = row_split[0]
+                        csv_writer.writerow([cik, company_name])
+                                
+                    except ValueError as e:
+                        continue
+                            
+            except(FileNotFoundError):
+                return("file_not_found")
+            
+            except(IndexError):
+                return("index error")
+
+    return save_path
+
+
+def match_company_names_to_cusip(collection_13f_file, save_file):
+    df = pandas.read_csv(collection_13f_file, header=0)
+    df = df.groupby(['cusip'])
+    
+    with open(save_file, 'w', newline='') as csv_header:
+        headerList = ['CIK','nameOfIssuer','Ticker']
+        csv_writer = csv.DictWriter(csv_header, delimiter=',', 
+                        fieldnames=headerList)
+        csv_writer.writeheader()
+        
+    with open(save_file, 'a+', newline='') as save_file:
+        for key, item in df:
+            csv_writer = csv.writer(save_file, delimiter=',',
+                escapechar=' ', quoting=csv.QUOTE_NONE)
+            ticker = get_source(key)
+            csv_writer.writerow([key, item['nameOfIssuer'].iloc[0], ticker])
+    
+    return save_file
+
+def merge_cik_to_cusip(collection_cusip, collection_cik, save_file):
+    cusip_df = pandas.read_csv(collection_cusip, header=0, index_col=['cusip'])
+    cik_df = pandas.read_csv(collection_cik, header=0, index_col=['nameOfIssuer'])
+    with open(save_file, 'a+', newline='') as save_file:
+        for key, item in cusip_df.iterrows():
+            #print(f"key: {key} item: {item['nameOfIssuer']}\n\n")
+            if item['nameOfIssuer'] in cik_df.index:
+                print(f"{item['nameOfIssuer']}, {item['Ticker']}, {cik_df._get_value(item['nameOfIssuer'], 'CIK')}")
+
+    #merged_df.to_csv(path_or_buf='merged_csv.csv')
+    
+    return save_file
+
 
 if __name__ == "__main__":
+    #company_ciks = read_all_copmany_names_from_edgar_master('Apps/Collection/src/resources/edgar-full-index-archives/master-2020-QTR1.txt', 
+                                                            #'company_cusip_name_ticker_collection.csv')
+    
     #clear_csv('13f_collection.csv')
     #file_arr = collect_13_f_companies_and_shares()
-    #file_13f = create_full_qtr_list_of_13f(file_arr)
+    #file_13f = create_full_qtr_list_of_13f(file_arr, '13f_collection.csv')
+    cusip_colection = match_company_names_to_cusip('13f_collection.csv', 'company_cusip_name_ticker_collection.csv')
+    merge_cik_to_cusip('company_cusip_name_ticker_collection.csv', 'cik_companyName.csv', 'merged.csv')
     
-    #    df['Total'] = df.groupby(['nameOfIssuer', 'cusip', 'sshPrnamtType', 'putCall', 'investmentDiscretion', 'otherManager', 'soleVotingAuthority', 'sharedVotingAuthority','noneVotingAuthority'])['value']['shares'].transform('sum')
 
-    df = pandas.read_csv('13f_collection.csv', header=0, index_col='Cusip')
-    df = df.groupby(['cusip'])
+    #df = pandas.read_csv(file_13f, header=0)
+    #print(list(df.columns))
+    #df['Total'] = df.groupby(['nameOfIssuer', 'cusip', 'sshPrnamtType', 'putCall', 'investmentDiscretion', 'otherManager', 'soleVotingAuthority', 'sharedVotingAuthority','noneVotingAuthority'])['value']['shares'].transform('sum')
 
-    with open('delete_me.txt', 'w', newline='') as txt_file:
-        for key, item in df:
-            txt_file.write(f"Key is: {(key)} \n head is : {df.head()}\n ")
-            #txt_file.write(f"{(item)}, \n\n")
+    #df = df.groupby(['cusip'])
+
+#     with open('delete_me.txt', 'w', newline='') as txt_file:
+#         for key, item in df:
+#             ticker = get_ticker(key)
+#             txt_file.write(f"Key is: {(key)} Ticker is: {ticker}\n")
+            
+#             txt_file.write(f"{(item)}, \n")
+#             try:
+#                 mean_val = item['shares'].sum() / item['value'].sum()
+            
+#             except ValueError as e:
+#                 print(f"value fail:  {item['shares'].sum()} / {item['value'].sum()}")
+                
+#             txt_file.write(f"shares sum : {item['shares'].sum()}  mean price in Thousands?: {mean_val}\n\n")
 
     #nameOfIssuer,cusip,value,shares,sshPrnamtType,putCall,investmentDiscretion,otherManager,soleVotingAuthority,sharedVotingAuthority,noneVotingAuthority
 

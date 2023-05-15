9b60d1581393946fd1e47782635490b8663481c5
maxeffortcode <bultman82@gmail.com>
2023-05-14 21:45:51 -0500

    final push of the night

--- Apps/Collection/src/api/file_request_handler.py
+++ Apps/Collection/src/api/file_request_handler.py
@@ -3,6 +3,7 @@ import itertools
 import time
 import sys
 import csv
+import traceback
 from sec_api import SecAPI
 from Settings.setup_logger import logging
 from Apps.Collection.src.helper import helper
@@ -55,70 +56,79 @@ class FileReqHandler:
                     continue
                 if line.split("|")[2] != filing_type:
                     lines.remove(line)
-            
-        for line in lines:
-            company_info_tuple = helper.get_company_info_tuple(line, self.quarter, self.year)
-            if(company_info_tuple[1] == "13F-HR"):
-                logger.info(
-                    f"Processing 13F-HR for : {company_info_tuple[0]}\n")
-                filing_response = sec_api.get_13f_hr_filing_for_company_api(
-                    company_info_tuple[4])
-                time.sleep(1/10)
-                helper().process_13f_hr(filing_response, company_info_tuple)
-            elif(company_info_tuple[1] == "13F-HR/A"):
-                logger.info(
-                    f"Processing 13F-HR/A for : {company_info_tuple[0]}\n")
-                filing_response = sec_api.get_13f_hr_filing_for_company_api(
-                    company_info_tuple[4])
-                time.sleep(1/10)
-                helper().process_13f_hr(filing_response, company_info_tuple)
-            elif(company_info_tuple[1] == "10-K"):
-                logger.info(
-                    f"Processing 10-K for : {company_info_tuple[0]}\n")
-                filing_response = sec_api.get_index_json_filing_response_for_company_api(
-                    company_info_tuple[4])
-                time.sleep(1/10)
-                helper.process_10k(
-                    filing_response, sec_api, company_info_tuple)
-            elif(company_info_tuple[1] == "10-K/A"):
-                logger.info(
-                    f"Processing 10-K/A for : {company_info_tuple[0]}\n")
-                filing_response = sec_api.get_index_json_filing_response_for_company_api(
-                    company_info_tuple[4])
-                time.sleep(1/10)
-                helper.process_10k(
-                    filing_response, sec_api, company_info_tuple)
-            elif(company_info_tuple[1] == "10-Q"):
-                logger.info(
-                    f"Processing 10-Q for : {company_info_tuple[0]}\n")
-                filing_response = sec_api.get_index_json_filing_response_for_company_api(
-                    company_info_tuple[4])
-                time.sleep(1/10)
-                helper.process_10q(
-                    filing_response, sec_api, company_info_tuple)
-            elif(company_info_tuple[1] == "10-Q/A"):
-                logger.info(
-                    f"Processing 10-Q/A for : {company_info_tuple[0]}\n")
-                filing_response = sec_api.get_index_json_filing_response_for_company_api(
-                    company_info_tuple[4])
-                time.sleep(1/10)
-                helper.process_10q(
-                    filing_response, sec_api, company_info_tuple)
-            else:
-                logger.info(
-                    f"Processing {company_info_tuple[1]} 'untracked' for : {company_info_tuple[0]}\n")
-                filing_response = sec_api.get_index_json_filing_response_for_company_api(
-                    company_info_tuple[4])
-                time.sleep(1/10)
-                helper.process_untracked(
-                    filing_response, sec_api, company_info_tuple)
-
-        #add in exception handling
         
+        #need to add a list to store the file paths
+        file_paths = []
+        try: 
+            for line in lines:
+                company_info_tuple = helper.get_company_info_tuple(line, self.quarter, self.year)
+                if(company_info_tuple[1] == "13F-HR"):
+                    logger.info(
+                        f"Processing 13F-HR for : {company_info_tuple[0]}\n")
+                    #goes to sec page and sets filing_response to the sec response
+                    filing_response = sec_api.get_13f_hr_filing_for_company_api(
+                        company_info_tuple[4])
+                    time.sleep(1/10)
+                    file_path = helper().process_13f_hr(filing_response, company_info_tuple)
+                    file_paths.append(file_path)
+                elif(company_info_tuple[1] == "13F-HR/A"):
+                    logger.info(
+                        f"Processing 13F-HR/A for : {company_info_tuple[0]}\n")
+                    filing_response = sec_api.get_13f_hr_filing_for_company_api(
+                        company_info_tuple[4])
+                    time.sleep(1/10)
+                    helper().process_13f_hr(filing_response, company_info_tuple)
+                elif(company_info_tuple[1] == "10-K"):
+                    logger.info(
+                        f"Processing 10-K for : {company_info_tuple[0]}\n")
+                    filing_response = sec_api.get_index_json_filing_response_for_company_api(
+                        company_info_tuple[4])
+                    time.sleep(1/10)
+                    helper.process_10k(
+                        filing_response, sec_api, company_info_tuple)
+                elif(company_info_tuple[1] == "10-K/A"):
+                    logger.info(
+                        f"Processing 10-K/A for : {company_info_tuple[0]}\n")
+                    filing_response = sec_api.get_index_json_filing_response_for_company_api(
+                        company_info_tuple[4])
+                    time.sleep(1/10)
+                    helper.process_10k(
+                        filing_response, sec_api, company_info_tuple)
+                elif(company_info_tuple[1] == "10-Q"):
+                    logger.info(
+                        f"Processing 10-Q for : {company_info_tuple[0]}\n")
+                    filing_response = sec_api.get_index_json_filing_response_for_company_api(
+                        company_info_tuple[4])
+                    time.sleep(1/10)
+                    helper.process_10q(
+                        filing_response, sec_api, company_info_tuple)
+                elif(company_info_tuple[1] == "10-Q/A"):
+                    logger.info(
+                        f"Processing 10-Q/A for : {company_info_tuple[0]}\n")
+                    filing_response = sec_api.get_index_json_filing_response_for_company_api(
+                        company_info_tuple[4])
+                    time.sleep(1/10)
+                    helper.process_10q(
+                        filing_response, sec_api, company_info_tuple)
+                else:
+                    logger.info(
+                        f"Processing {company_info_tuple[1]} 'untracked' for : {company_info_tuple[0]}\n")
+                    filing_response = sec_api.get_index_json_filing_response_for_company_api(
+                        company_info_tuple[4])
+                    time.sleep(1/10)
+                    helper.process_untracked(
+                        filing_response, sec_api, company_info_tuple)
+            
+        except Exception as e:
+            logger.error(f"Error processing filing for {company_info_tuple[0]}")
+            logger.error(e)
+            logger.error(traceback.format_exc())
+            
         #return the file path
+        
 
 if __name__ == "__main__":
-    file_req_handler = FileReqHandler("2018", "1", "1000045")
+    file_req_handler = FileReqHandler("2017", "1", "1000045")
     
     #get the file
     file_req_handler.get_file()
